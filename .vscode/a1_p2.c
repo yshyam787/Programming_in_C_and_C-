593d9f741a7b21ced15461cdc9c53badb0b61eb9
yshyam787 <yshyam787@gmail.com>
2021-09-11 12:15:18 +0200

    solved a1.2

--- .vscode/a1_p2.c
+++ .vscode/a1_p2.c
@@ -1,4 +1,15 @@
-#include<stdio.h>
-int main(){
-    printf("name");
-}
+/*
+CH-230-A
+a1 p1.[c]
+Shyam Yadav
+s.yadav@jacobs-university.de
+*/
+#include <stdio.h>
+int main() {
+    int result; /*The result of our calculation*/
+    result=(2+7)*9/3; 
+    printf("The result is %d\n", result); 
+    return 0;
+}   
+
+/*In the given program we are expecting the integer result, however the argument was not provided in the printf function. Therefore,the compiler was giving us the garbage integer result. It was fixed by prividing the suitable argument "result" in the printf function.*/

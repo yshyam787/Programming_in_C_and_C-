83839c324fb85c53639e0ac62677c21b32885f5d
yshyam787 <yshyam787@gmail.com>
2021-09-11 13:40:16 +0200

    solving a1_p4

---
+++ .vscode/Hw_first/a1_p4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int x = 17, y = 4;
+    printf("x=%d\n", x);
+    printf("y=%d\n", y);
+    int result = x + y;
+    printf("sum=%d\n", result);
+    printf("product=%d\n", x * y);
+    printf("difference=%d\n", x - y);
+    double division = (double) x / (double) y; //converting variable of datatype int to double. 
+    printf("division=%f\n", division);
+    printf("remainder of division=%d\n", x%y);
+}

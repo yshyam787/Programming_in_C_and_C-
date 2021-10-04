460c2eeaf665f3c65de5ad7ce43a1e433567f3cf
yshyam787 <yshyam787@gmail.com>
2021-10-04 14:02:53 +0200

    new_assignements

---
+++ Hw_third/Hw_fourth/a4_p2.c
@@ -0,0 +1,21 @@
+/*
+CH-230-A
+a4 p2.[c]
+Shyam Yadav
+s.yadav@jacobs-university.de
+*/
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char string[50];
+    fgets(string, sizeof(string), stdin);
+
+    //printing zigzag word.
+    for (int i = 0; i < strlen(string); i++){
+        printf("%c\n", string[i]);
+        printf(" %c\n", string[i+1]);
+        i += 1;
+    }
+    return 0;
+}
